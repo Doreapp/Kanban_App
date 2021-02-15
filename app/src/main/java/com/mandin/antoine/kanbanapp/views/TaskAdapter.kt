b84b1be969f7955c43b95244f62f2d17a8a65b4f
Doreapp <Doreapp@users.noreply.github.com>
2021-02-15 15:59:18 +0100

    Fix bug Delete. Fix bug edition (related to move before edit). Change organisation (move TaskViewHolder to a indenpendant file + Adapter implements directly observer interface). Close database on app end.

--- app/src/main/java/com/mandin/antoine/kanbanapp/views/TaskAdapter.kt
+++ app/src/main/java/com/mandin/antoine/kanbanapp/views/TaskAdapter.kt
@@ -3,17 +3,12 @@ package com.mandin.antoine.kanbanapp.views
 import android.annotation.SuppressLint
 import android.util.Log
 import android.view.LayoutInflater
-import android.view.MotionEvent
-import android.view.View
-import android.view.View.GONE
-import android.view.View.VISIBLE
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.mandin.antoine.kanbanapp.R
 import com.mandin.antoine.kanbanapp.model.Label
 import com.mandin.antoine.kanbanapp.model.TaskWithLabels
-import kotlinx.android.synthetic.main.view_task.view.*
 
 /**
  * Adapter displaying task views
@@ -24,9 +19,9 @@ class TaskAdapter
     private val modificationSaver: ModificationSaver,
     private val startDragListener: TaskTouchHelperCallback.OnStartDragListener
 ) :
-    RecyclerView.Adapter<TaskAdapter.TaskViewHolder>(),
-    TaskTouchHelperCallback.ItemTouchHelperAdapter,
-    TaskObserver {
+    RecyclerView.Adapter<TaskViewHolder>(),
+    TaskTouchHelperCallback.TaskTouchHelperAdapter,
+    TaskViewHolderObserver {
     /**
      * Current editing item index in list [tasks], -1 if none
      */
@@ -38,83 +33,6 @@ class TaskAdapter
      */
     private var newOne = false
 
-    /**
-     * Implementation on listener of task editing end
-     */
-    /**
-     * Callback for cancellation of task edit
-     */
-    override fun onTaskEditCanceled(task: TaskWithLabels?) {
-        log("onTaskEditCanceled (task=$task, editingItem=$editingItem, newOne=$newOne)")
-        val oldEditingItem = editingItem
-        editingItem = -1
-        if (newOne) {
-            newOne = false
-            notifyItemRemoved(oldEditingItem)
-        } else {
-            notifyItemChanged(oldEditingItem)
-        }
-    }
-
-    /**
-     * Callback for validation of task edit
-     */
-    override fun onTaskEditSaved(
-        task: TaskWithLabels?, title: String, description: String,
-        labels: List<Label>
-    ) {
-        log("onTaskEditSaved (task=$task, editingItem=$editingItem, newOne=$newOne, title=$title, description=$description, labels=$labels)")
-        if (task == null) {
-            //Creating
-            val priority = if (tasks.isEmpty()) 0 else tasks[0].task.priority + 1
-            val task = modificationSaver.createNewTask(title, description, labels, priority)
-            tasks.add(0, task)
-
-            newOne = false
-            editingItem = -1
-            notifyItemChanged(0)
-        } else {
-            task.task.title = title
-            task.task.description = description
-            task.labels = labels
-            modificationSaver.saveTaskChanges(task)
-
-            val oldEditingItem = editingItem
-            editingItem = -1
-            notifyItemChanged(oldEditingItem)
-        }
-    }
-
-    override fun onTaskRemoved(task: TaskWithLabels?) {
-        log("onTaskRemoved task=$task")
-        task?.let {
-            modificationSaver.deleteTask(it)
-        }
-    }
-
-    override fun onTaskMoveRight(task: TaskWithLabels) {
-        log("onTaskMoveRight task=$task")
-        if (modificationSaver.moveTaskRight(task)) {
-            val index = tasks.indexOf(task)
-            tasks.removeAt(index)
-            notifyItemRemoved(
-                index + if (newOne) 1 else 0
-            )
-        }
-    }
-
-    override fun onTaskMoveLeft(task: TaskWithLabels) {
-        log("onTaskMoveLeft task=$task")
-        if (modificationSaver.moveTaskLeft(task)) {
-            val index = tasks.indexOf(task)
-            tasks.removeAt(index)
-            notifyItemRemoved(
-                index + if (newOne) 1 else 0
-            )
-        }
-    }
-
-
     private fun log(str: String) {
         Log.i("TaskAdapter", str)
     }
@@ -125,10 +43,9 @@ class TaskAdapter
         }
     }
 
-
     fun addNewTask() {
         log("addNewTask")
-        if(newOne)
+        if (newOne)
             return
 
         newOne = true
@@ -138,21 +55,14 @@ class TaskAdapter
 
     fun insertOnTop(task: TaskWithLabels) {
         log("insertOnTop (task=$task)")
-        if (editingItem >= 0) {
-            if (newOne) {
-                newOne = false
-                editingItem = -1
-                notifyItemRemoved(0)
-            } else {
-                val oldEditingItem = editingItem
-                editingItem = -1
-                notifyItemChanged(oldEditingItem)
-            }
-        }
 
-        val priority = if (tasks.isEmpty()) 0 else tasks[0].task.priority + 1
+        val priority = if (tasks.isEmpty()) 0 else (tasks[0].task.priority + 1)
         task.task.priority = priority
         tasks.add(0, task)
+        if (editingItem >= 0 && !newOne) {
+            // We are editing an item already persisted. The task will be inserted before.
+            editingItem += 1
+        }
         notifyItemInserted(if (newOne) 1 else 0)
     }
 
@@ -167,32 +77,8 @@ class TaskAdapter
         if (newOne && position == 0) {
             holder.update(null)
         } else {
-            holder.update(tasks[position])
+            holder.update(tasks[position + if (newOne) -1 else 0])
             holder.editing = position == editingItem
-        }
-        holder.itemView.btnEdit.setOnClickListener {
-            if (position != editingItem && !newOne && editingItem < 0) {
-                editingItem = position
-                notifyItemChanged(position)
-            }
-        }
-        holder.itemView.btnReorder.setOnTouchListener { _, event ->
-            when (event.actionMasked) {
-                MotionEvent.ACTION_DOWN -> {
-                    holder.onItemSelected()
-                    startDragListener.onStartDrag(holder)
-                }
-            }
-            false
-        }
-        holder.itemView.btnMoveHorizontally.setOnTouchListener { _, event ->
-            when (event.actionMasked) {
-                MotionEvent.ACTION_DOWN -> {
-                    holder.onItemSelected()
-                    startDragListener.onStartSwipe(holder)
-                }
-            }
-            false
         }
     }
 
@@ -200,135 +86,148 @@ class TaskAdapter
         return tasks.size + if (newOne) 1 else 0
     }
 
-    override fun onItemMove(fromPosition: Int, toPosition: Int): Boolean {
-        log("onItemMove fromPosition=$fromPosition, toPosition=$toPosition)")
-        if (fromPosition < tasks.size && toPosition < tasks.size) {
-            val offset = if (newOne) -1 else 0
-            val movedItem = tasks[fromPosition + offset]
-            val toItem = tasks[toPosition + offset]
-            movedItem.task.priority = toItem.task.priority
+    override fun onItemMove(viewHolder: TaskViewHolder, toPosition: Int): Boolean {
+        log("onItemMove viewHolder=$viewHolder, toPosition=$toPosition")
+        val fromPosition = viewHolder.adapterPosition
+        val offset = if (newOne) -1 else 0
+        val movedItem = viewHolder.task!!
+        val toItem = tasks[toPosition + offset]
+        movedItem.task.priority = toItem.task.priority
 
-            if (fromPosition < toPosition) {
-                for (i in fromPosition until toPosition) {
-                    tasks[i].task.priority++
-                }
-                tasks.removeAt(fromPosition + offset)
-                tasks.add(toPosition - 1 + offset, movedItem)
-            } else {
-                for (i in fromPosition downTo toPosition + 1) {
-                    tasks[i].task.priority--
-                }
-                tasks.removeAt(fromPosition + offset)
-                tasks.add(toPosition + offset, movedItem)
+        log("onItemMove, before list edit: tasks=$tasks")
+        if (fromPosition < toPosition) {
+            for (i in fromPosition+1..toPosition) {
+                tasks[i].task.priority++
             }
-
-            notifyItemMoved(fromPosition, toPosition)
+            tasks.removeAt(fromPosition + offset)
+            tasks.add(toPosition + offset, movedItem)
+        } else {
+            for (i in toPosition until fromPosition) {
+                tasks[i].task.priority--
+            }
+            tasks.removeAt(fromPosition + offset)
+            tasks.add(toPosition + offset, movedItem)
         }
+        log("onItemMove, after list edit: tasks=$tasks")
+
+        notifyItemMoved(fromPosition, toPosition)
+
         return true
     }
 
-    override fun onItemDismiss(position: Int) {
-        log("onItemDismiss (position=$position)")
+    override fun onItemDismiss(viewHolder: TaskViewHolder) {
+        log("onItemDismiss (viewHolder=$viewHolder)")
+        viewHolder.onItemClear()
         //TODO, in function of the direction (right of left), we may move the item
     }
 
-    override fun onItemSwiped(position: Int, direction: Int) {
-        log("onItemSwiped position=$position, direction=$direction")
-        if (position == 0 && newOne) {
+    override fun onItemSwiped(viewHolder: TaskViewHolder, direction: Int) {
+        log("onItemSwiped viewHolder=$viewHolder, direction=$direction")
+        if (viewHolder.editing || newOne) {
             // Try to move the editing new One
-            editingItem = -1
-            newOne = false
-            notifyItemRemoved(0)
+            Log.e("TaskAdapter", "Huston, We got a problem : swiped and editing item.", Throwable())
         } else {
-            if (position == editingItem)
-                editingItem = -1
-
-            val offset = if (newOne) -1 else 0
             when (direction) {
-                ItemTouchHelper.END -> onTaskMoveRight(tasks[position+offset])
-                ItemTouchHelper.START -> onTaskMoveLeft(tasks[position+offset])
+                ItemTouchHelper.END -> moveTaskRight(viewHolder)
+                ItemTouchHelper.START -> moveTaskLeft(viewHolder)
             }
         }
     }
 
+    override fun startSwipingTask(taskViewHolder: TaskViewHolder) {
+        log("startSwipingTask taskViewHolder=$taskViewHolder")
+        taskViewHolder.onItemSelected()
+        startDragListener.onStartSwipe(taskViewHolder)
+    }
 
-    class TaskViewHolder(
-        itemView: View,
-        val taskObserver: TaskObserver
-    ) : RecyclerView.ViewHolder(itemView),
-        TaskTouchHelperCallback.ItemTouchHelperViewHolder {
-        private var task: TaskWithLabels? = null
-        var editing = false
-            set(value) {
-                field = value
-                itemView.etDescription.isEnabled = value
-                itemView.etTitle.isEnabled = value
-                if (value) {
-                    itemView.etDescription.visibility = VISIBLE
-                    itemView.layoutButtons.visibility = VISIBLE
-                    itemView.btnDelete.visibility = VISIBLE
-                    itemView.btnEdit.visibility = GONE
-                    itemView.btnMoveHorizontally.visibility = GONE
-                } else {
-                    itemView.layoutButtons.visibility = GONE
-                    itemView.btnDelete.visibility = GONE
-                    itemView.btnEdit.visibility = VISIBLE
-                    itemView.btnMoveHorizontally.visibility = VISIBLE
-                }
-            }
+    override fun startReorderingTask(taskViewHolder: TaskViewHolder) {
+        log("startReorderingTask taskViewHolder=$taskViewHolder")
+        taskViewHolder.onItemSelected()
+        startDragListener.onStartDrag(taskViewHolder)
+    }
 
-        init {
-            itemView.btnCancel.setOnClickListener {
-                taskObserver.onTaskEditCanceled(task)
-            }
-            itemView.btnSave.setOnClickListener {
-                taskObserver.onTaskEditSaved(
-                    task,
-                    itemView.etTitle.text.toString(),
-                    itemView.etDescription.text.toString(),
-                    ArrayList() // TODO labels here
-                )
-            }
-            itemView.btnDelete.setOnClickListener {
-                taskObserver.onTaskRemoved(task)
+
+    override fun startEditingTask(taskViewHolder: TaskViewHolder) {
+        log("startEditingTask taskViewHolder=$taskViewHolder")
+        val position = taskViewHolder.adapterPosition
+        editingItem = position
+        notifyItemChanged(position)
+    }
+
+    /**
+     * Callback for cancellation of task edit
+     */
+    override fun cancelTaskEdition(taskViewHolder: TaskViewHolder) {
+        log("onTaskEditCanceled (taskViewHolder=$taskViewHolder, editingItem=$editingItem, newOne=$newOne)")
+        val oldEditingItem = editingItem
+        editingItem = -1
+        if (newOne) {
+            newOne = false
+            notifyItemRemoved(oldEditingItem)
+        } else {
+            notifyItemChanged(oldEditingItem)
+        }
+    }
+
+    /**
+     * Callback for validation of task edit
+     */
+    override fun saveTaskEdition(
+        taskViewHolder: TaskViewHolder,
+        title: String, description: String,
+        labels: List<Label>
+    ) {
+        log("onTaskEditSaved (taskViewHolder=$taskViewHolder, editingItem=$editingItem, newOne=$newOne, title=$title, description=$description, labels=$labels)")
+        if (taskViewHolder.task == null) {
+            //Creating
+            val priority = if (tasks.isEmpty()) 0 else tasks[0].task.priority + 1
+            val task = modificationSaver.createNewTask(title, description, labels, priority)
+            tasks.add(0, task)
+
+            newOne = false
+            editingItem = -1
+            notifyItemChanged(0)
+        } else {
+            val task = taskViewHolder.task!!
+            task.task.title = title
+            task.task.description = description
+            task.labels = labels
+            modificationSaver.saveTaskChanges(task)
+
+            val oldEditingItem = editingItem
+            editingItem = -1
+            notifyItemChanged(oldEditingItem)
+        }
+    }
+
+    override fun removeTask(taskViewHolder: TaskViewHolder) {
+        log("onTaskRemoved task=$taskViewHolder")
+        taskViewHolder.task?.let {
+            modificationSaver.deleteTask(it)
+            tasks.remove(it)
+            val oldPosition = editingItem
+            editingItem = -1
+            notifyItemRemoved(oldPosition)
+        }
+    }
+
+    override fun moveTaskRight(taskViewHolder: TaskViewHolder) {
+        log("onTaskMoveRight taskViewHolder=$taskViewHolder")
+        taskViewHolder.task?.let {
+            if (modificationSaver.moveTaskRight(it)) {
+                tasks.remove(it)
+                notifyItemRemoved(taskViewHolder.adapterPosition)
             }
         }
+    }
 
-        fun update(newTask: TaskWithLabels?) {
-            this.task = newTask
-            if (task != null) {
-                itemView.etTitle.setText(task!!.task.title)
-                if (task!!.task.description.isNotBlank()) {
-                    itemView.etDescription.visibility = VISIBLE
-                    itemView.etDescription.setText(task!!.task.description)
-                } else {
-                    itemView.etDescription.visibility = GONE
-                }
-            } else {
-                // New one
-                itemView.etTitle.setText("")
-                itemView.etDescription.visibility = VISIBLE
-                itemView.etDescription.setText("")
-                editing = true
+    override fun moveTaskLeft(taskViewHolder: TaskViewHolder) {
+        log("onTaskMoveLeft task=$taskViewHolder")
+        taskViewHolder.task?.let {
+            if (modificationSaver.moveTaskLeft(it)) {
+                tasks.remove(it)
+                notifyItemRemoved(taskViewHolder.adapterPosition)
             }
-
-            // TODO labels
-        }
-
-        private fun log(str: String) {
-            Log.i("TaskViewHolder", str)
-        }
-
-        override fun onItemSelected() {
-            log("onItemSelected (position=$adapterPosition)")
-            //itemView.isSelected = true
-            //itemView.btnReorder.isPressed = true
-        }
-
-        override fun onItemClear() {
-            log("onItemClear (position=$adapterPosition)")
-            //itemView.isSelected = false
-            //itemView.btnReorder.isPressed = false
         }
     }
 
---
+++ app/src/main/java/com/mandin/antoine/kanbanapp/views/TaskViewHolder.kt
@@ -0,0 +1,194 @@
+package com.mandin.antoine.kanbanapp.views
+
+import android.util.Log
+import android.view.MotionEvent
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.mandin.antoine.kanbanapp.model.TaskWithLabels
+import kotlinx.android.synthetic.main.view_task.view.*
+
+/**
+ * View holder of a [TaskWithLabels]
+ */
+class TaskViewHolder(
+    itemView: View,
+    private val taskViewHolderObserver: TaskViewHolderObserver
+) : RecyclerView.ViewHolder(itemView),
+    TaskTouchHelperCallback.ItemTouchHelperViewHolder {
+    var task: TaskWithLabels? = null
+
+    /**
+     * Setter of [editing]
+     * If currently editing the task
+     */
+    var editing = false
+        set(value) {
+            field = value
+            // Enable or disable in function
+            itemView.etDescription.isEnabled = value
+            itemView.etDescription.setText(task?.task?.description)
+            itemView.etTitle.isEnabled = value
+            itemView.etTitle.setText(task?.task?.title)
+            if (value) {
+                // Show description edit (even if empty) + buttons of save/cancel modifications + delete button
+                itemView.etDescription.visibility = View.VISIBLE
+                itemView.layoutButtons.visibility = View.VISIBLE
+                itemView.btnDelete.visibility = View.VISIBLE
+                // Hide `edit`, `move horizontally` and `ReOrder` buttons
+                itemView.btnEdit.visibility = View.GONE
+                itemView.btnMoveHorizontally.visibility = View.GONE
+                itemView.btnReorder.visibility = View.GONE
+            } else {
+                // Hide buttons of save/cancel modifications + delete button
+                itemView.layoutButtons.visibility = View.GONE
+                itemView.btnDelete.visibility = View.GONE
+                // Show `edit`, `move horizontally` and `ReOrder` buttons
+                itemView.btnEdit.visibility = View.VISIBLE
+                itemView.btnMoveHorizontally.visibility = View.VISIBLE
+                itemView.btnReorder.visibility = View.VISIBLE
+            }
+        }
+
+    private fun log(str: String) {
+        Log.i("TaskViewHolder", str)
+    }
+
+    init {
+        // On cancel edit click
+        itemView.btnCancel.setOnClickListener {
+            cancel()
+        }
+        itemView.btnSave.setOnClickListener {
+            save()
+        }
+        itemView.btnDelete.setOnClickListener {
+            remove()
+        }
+        itemView.btnEdit.setOnClickListener {
+            edit()
+        }
+        itemView.btnReorder.setOnTouchListener { _, event ->
+            when (event.actionMasked) {
+                MotionEvent.ACTION_DOWN -> {
+                    startReordering()
+                }
+            }
+            false
+        }
+        itemView.btnMoveHorizontally.setOnTouchListener { _, event ->
+            when (event.actionMasked) {
+                MotionEvent.ACTION_DOWN -> {
+                    startSwiping()
+                }
+            }
+            false
+        }
+    }
+
+    /**
+     * Cancel the modifications made during edition,
+     * should be call only when [editing] is true
+     */
+    fun cancel() {
+        log("cancel(). Editing=$editing")
+        taskViewHolderObserver.cancelTaskEdition(this)
+    }
+
+    /**
+     * Cancel the modifications made during edition,
+     * should be call only when [editing] is true
+     * TODO -> Give labels to function, in order to save those
+     */
+    fun save() {
+        log("save(). Editing=$editing")
+        taskViewHolderObserver.saveTaskEdition(
+            this,
+            itemView.etTitle.text.toString(),
+            itemView.etDescription.text.toString(),
+            ArrayList()
+        )
+    }
+
+    /**
+     * Remove the task
+     * should be call only when [editing] is true
+     */
+    fun remove() {
+        log("remove(). Editing=$editing")
+        taskViewHolderObserver.removeTask(this)
+    }
+
+    /**
+     * Start editing the task
+     * should be call only when [editing] is false
+     */
+    fun edit() {
+        log("edit(). Editing=$editing, task=$task")
+        taskViewHolderObserver.startEditingTask(this)
+    }
+
+    /**
+     * Start reordering the task
+     * should be call only when [editing] is false
+     */
+    fun startReordering() {
+        log("startReordering(). Editing=$editing, task=$task")
+        taskViewHolderObserver.startReorderingTask(this)
+    }
+
+    /**
+     * Start swiping the task holder
+     * should be call only when [editing] is false
+     */
+    fun startSwiping() {
+        log("startSwiping(). Editing=$editing, task=$task")
+        taskViewHolderObserver.startSwipingTask(this)
+
+    }
+
+    /**
+     * Update the display with the new task ([newTask]) to show.
+     * If [newTask] is null, then the view holder represent a new task, not persisted in the database yet
+     */
+    fun update(newTask: TaskWithLabels?) {
+        if(newTask == task) {
+            log("update() position=$adapterPosition, task stays the same")
+            return
+        }
+        log("update() position=$adapterPosition, task become $newTask")
+        this.task = newTask
+        if (task != null) {
+            itemView.etTitle.setText(task!!.task.title)
+            if (task!!.task.description.isNotBlank()) {
+                itemView.etDescription.visibility = View.VISIBLE
+                itemView.etDescription.setText(task!!.task.description)
+            } else {
+                itemView.etDescription.visibility = View.GONE
+            }
+        } else {
+            // New task
+            itemView.etTitle.setText("")
+            itemView.etDescription.visibility = View.VISIBLE
+            itemView.etDescription.setText("")
+            editing = true
+        }
+
+        // TODO labels
+    }
+
+    override fun toString(): String {
+        return "TaskViewHolder{editing=$editing, task=$task, adapterPosition=$adapterPosition}"
+    }
+
+    override fun onItemSelected() {
+        log("onItemSelected (position=$adapterPosition)")
+        //itemView.isSelected = true
+        //itemView.btnReorder.isPressed = true
+    }
+
+    override fun onItemClear() {
+        log("onItemClear (position=$adapterPosition)")
+        //itemView.isSelected = false
+        //itemView.btnReorder.isPressed = false
+    }
+}
